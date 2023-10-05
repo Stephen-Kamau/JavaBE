0f58313946b217df2ad2aa1117ef81340a9bcceb
Stephen-Kamau <stiveckamash@gmail.com>
2023-10-05 20:54:39 +0300

    updated

---
+++ SecureEndpointsJwt/src/test/java/com/example/secureendpointsjwt/SecureEndpointsJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.secureendpointsjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureEndpointsJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
